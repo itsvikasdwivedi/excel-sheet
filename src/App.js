95658264a4e8164b790c04343f9f23abd99c6fca
itsvikasdwivedi <vickyserver22@gmail.com>
2023-03-28 18:21:50 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DataGrid from './Component/TableComp';
+
+function App() {
+  return (
+    <div className="App">
+     <DataGrid/>
+    </div>
+  );
+}
+
+export default App;
